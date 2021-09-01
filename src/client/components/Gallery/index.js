b42003f54300723b7336afba9cc2945f910cfbf3
Maxcrazy1 <serranocristian9@gmail.com>
2021-09-01 08:58:52 -0400

    Style: Fix scroll in home page

--- src/client/components/Gallery/index.js
+++ src/client/components/Gallery/index.js
@@ -5,7 +5,7 @@ import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   "@global": {
-    body: {
+    "#root": {
       overflow: "hidden !important",
 
     },
